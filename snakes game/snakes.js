b2b712ac8b66b33d50e358153a9c08df6b7bb6fb
Prateek Sharan <72613286+PrateekSharan@users.noreply.github.com>
2022-09-22 02:10:29 +0530

    speed control

--- snakes game/snakes.js
+++ snakes game/snakes.js
@@ -1,7 +1,9 @@
 // Snakes Game
+// ~Prateek
+
+// Initializing elements
 
 function init(){
-    
     canvas = document.getElementById('mycanvas');
     pen = canvas.getContext('2d');
     W = canvas.width;
@@ -14,7 +16,7 @@ function init(){
     
     snake = {
         init_length:5,
-        color:"yellow",
+        color:"green",
         cells:[],
         direction:"right",
         
@@ -26,8 +28,7 @@ function init(){
         drawSnake:function(){
             
             for(var i=0;i<this.cells.length;i++){
-                pen.fillStyle = this.color;
-                
+                pen.fillStyle = this.color; 
                 pen.strokeStyle = "black";
                 pen.lineWidth  = 5;
                 
@@ -35,8 +36,6 @@ function init(){
             }
             
         },
-
-        // Update the snake
         updateSnake:function(){
             var headX = this.cells[0].x;
             var headY = this.cells[0].y;
@@ -48,7 +47,8 @@ function init(){
             else{
                 //Pop last cell if food not eaten
                 this.cells.pop();
-            }
+            }      
+            
             
             if(this.direction =="right"){
                 nextX = headX + 1;
@@ -62,11 +62,10 @@ function init(){
                 nextX = headX;
                 nextY = headY + 1;
             }
-            else if(this.direction=="up"){
+            else if(this.direction="up"){
                 nextX = headX;
                 nextY = headY - 1;
             }
-
             //Insert the new cell at head/front
             this.cells.unshift({x:nextX,y:nextY});
             
@@ -75,13 +74,12 @@ function init(){
             var last_x = Math.round(W/10);
             var last_y = Math.round(H/10);
             
-            if(this.cells[0].y<0 || this.cells[0].x <0|| this.cells[0].x>last_x || this.cells[0].y>last_y){
-                    alert("GameOver!! Your score is: " + score);
-                    game_over = true;
-                
+            if(this.cells[0].y<0 || this.cells[0].x<0|| this.cells[0].x>last_x || this.cells[0].y>last_y){
+                    if(game_over==false){
+                        alert("GameOver, Your score is: " + score);
+                        game_over = true;
+                    }
             }
-            
-            
             
         }
     };
@@ -91,24 +89,20 @@ function init(){
     
     function KeyPressed(e){
         
-        console.log("You pressed a key");
-        console.log(e);
-        
-        if(e.key=="ArrowRight" || e.key=="D" || e.key=="d"){
+        if(e.key=="ArrowRight" || e.key=="d" || e.key=="D"){
             snake.direction = "right";
         }
-        else if(e.key=="ArrowLeft" || e.key=="A" || e.key=="a"){
+        else if(e.key=="ArrowLeft" || e.key=="a" || e.key=="A"){
             snake.direction = "left";
         }
-        else if(e.key=="ArrowDown" || e.key=="S" || e.key=="s"){
+        else if(e.key=="ArrowDown" || e.key=="s" || e.key=="S"){
             snake.direction = "down";
         }
-        else if(e.key=="ArrowUp" || e.key=="W" || e.key=="w"){
+        else if(e.key=="ArrowUp" || e.key=="w" || e.key=="W"){
             snake.direction = "up";
         }
         
     }
-    
     
     document.addEventListener('keydown',KeyPressed);
     
@@ -117,8 +111,7 @@ function init(){
 function draw(){
     pen.clearRect(0,0,W,H);
     snake.drawSnake();
-    
-    //Lets us draw the food
+    //console.log("In draw");
     
     pen.fillStyle = food.color;
     
@@ -142,15 +135,15 @@ function gameLoop(){
     update();
     
     if(game_over==true){
-        clearInterval(f);
+        clearInterval(speed);
     }
 }
 
 function getRandomFood(){
-    var foodX = Math.round(Math.random()*(W-10)/10);
-    var foodY = Math.round(Math.random()*(H-10)/10);
+    var foodX = Math.round(Math.random()*(this.W-10)/10);
+    var foodY = Math.round(Math.random()*(this.H-10)/10);
     
-    foodColors = ["red","green","aqua","coral","orchid"];
+    var foodColors = ["red","yellow","aqua","coral","orchid","white"];
     var i = Math.round(Math.random()*foodColors.length);
     
     
@@ -164,12 +157,17 @@ function getRandomFood(){
     return food;
 }
 
+// Determine Speed of the game
+
+function speedslow(){
+    speed = setInterval(gameLoop,150);
+}
+
+function speedmedium(){
+    speed = setInterval(gameLoop,100);
+}
+function speedfast(){
+    speed = setInterval(gameLoop,70);
+}
+
 init();
-
-
-//Speed of the game:
-
-var f = setInterval(gameLoop,100);
-
-
-
